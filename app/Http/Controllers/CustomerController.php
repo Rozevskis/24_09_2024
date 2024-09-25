e1fe221a0bdffdb4f243dfdae45d82e4c2dae6fc
Kristofers Jēkabs Rozevskis <kristofers.rozevskis@gmail.com>
2024-09-25 12:49:48 +0300

    Index implemented with querry builder

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,17 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Customer;
+use Illuminate\Support\Facades\DB;
 
 
 class CustomerController extends Controller
 {
     
+    
     public function index()
     {
-        $allCustomers = Customer::all();
-        return $allCustomers;
-    }
+        $customersWithOrders = DB::table('customers as c')
+            ->join('orders as o', 'c.customer_id', '=', 'o.customer_id')
+            ->join('order_statuses as os', 'o.status', '=', 'os.order_status_id')
+            ->select(
+                'c.customer_id',
+                'c.first_name',
+                'c.last_name',
+                'c.address',
+                'c.city',
+                'c.state',
+                'c.points',
+                'o.order_date',
+                'os.name as order_status_name'
+            )
+            ->get();
 
+        return response()->json($customersWithOrders);
+    }
+    
     public function store(Request $request)
     {
         $data = $request->validate([
